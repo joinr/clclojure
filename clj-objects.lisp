c2a3a2c8ea47d4e810232e594d3ffd137a29c579
tom <tom@the-cloud>
2025-11-13 22:58:17 +0000

    modified naming conventions to conform with legacy junk, maybe we change later.

--- clj-objects.lisp
+++ clj-objects.lisp
@@ -4,20 +4,19 @@
 ;;persistent vector structs etc.  We can treat these as superclasses
 ;;for our implementation level stuff and e.g. pack along metadata and
 ;;hasheq slots with our core datastructures.
-
 (defpackage :clj-objects ;;might change this to clojure.lang at some point.
-  (:use :common-lisp :common-utils)
+  (:use :common-lisp)
   (:export :make-cljstruct))
 (in-package :clj-objects)
 
-(defstruct cljstruct (meta nil) (hash -1))
+(defstruct cljstruct (_meta nil) (_hasheq -1))
 ;;do we need these? hmm maybe.
 (defclass  cljclass ()
-  ((meta
-    :initarg :meta
+  ((_meta
+    :initarg :_meta
     :initform nil
-    :accessor meta)
-   (hash
-    :initarg :hash
+    :accessor _meta)
+   (_hasheq
+    :initarg :_hasheq
     :initform -1
-    :accessor hash)))
+    :accessor _hasheq)))
